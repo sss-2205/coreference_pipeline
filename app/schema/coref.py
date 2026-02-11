90bc9949a6f66c122d8f91e0d1293284f4fe4754
sss-2205 <shahid.sayyad2205@gmail.com>
2026-02-11 12:49:13 +0530

    Structured response for sentence categorization of article

--- app/schema/coref.py
+++ app/schema/coref.py
@@ -1,4 +1,11 @@
+from xxlimited import Str
 from pydantic import BaseModel, HttpUrl
+from typing import List
+
+class item(BaseModel):
+    sent: str | None = None
+    label: str | None = None
+     
 
 class Article(BaseModel): # the schema for the input request for coreference api. change this according to orchestration method
 
@@ -11,4 +18,4 @@ class Coref_Article(BaseModel): # the schema for the input request for preproces
     content: str | None = None
     url: HttpUrl
     chains: list | None = None # this will hold the coreference chains in string format. change this according to orchestration method   
-    
+    ner_list: list[item] | None = None # this will hold the ner list in string format. change this according to orchestration method
